b48b6ca9e64c2a6881985e5c52ed62521609c6be
whatiskeptiname <susangmree@gmail.com>
2026-05-07 13:22:24 -0400

    added script for power menu

---
+++ sway/scripts/powermenu.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+lock="Lock"
+shutdown="Shutdown"
+suspend="Suspend"
+hibernate="Hibernate"
+reboot="Reboot"
+exit_sway="Exit Sway"
+
+chosen=$(printf "%s\n%s\n%s\n%s\n%s\n%s" \
+    "$lock" "$shutdown" "$reboot" "$suspend" "$hibernate" "$exit_sway" \
+    | rofi -dmenu -p "Power" -i)
+
+case "$chosen" in
+    "$lock")     swaylock -f -c 000000 ;;
+    "$shutdown") systemctl poweroff ;;
+    "$suspend")  systemctl suspend ;;
+    "$hibernate") systemctl hibernate ;;
+    "$reboot")   systemctl reboot ;;
+    "$exit_sway") swaymsg exit ;;
+esac
